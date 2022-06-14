b90bdccc411f9d1f1cd8473e4a6b10d710e5f8fb
HugoBosy <hkliszcz@gmail.com>
2022-06-14 18:01:08 +0200

    add tests latest product & styles display

--- src/pages/Dasboard/Dashboard.tsx
+++ src/pages/Dasboard/Dashboard.tsx
@@ -6,6 +6,7 @@ export const Dashboard = () => {
     return (
         <div className="page">
             <h1>Dashboard</h1>
+
             <div className="Dashboard__base-info">
                 <div className="Dashboard__base-item">
                     <span className="Dashboard__base-icon"><Basket size="30"/></span>
@@ -38,6 +39,37 @@ export const Dashboard = () => {
                         <p>(ilość uzytkowników)</p>
                         <p>Ostatnio zarejestrowany uzytkownik</p>
                     </div>
+                </div>
+            </div>
+
+            <div className="Dashboard__latest-sell">
+                <h2>Najlepiej sprzedajace się produkty</h2>
+                <div className="Dashboard__latest-item">
+                    <p>Photo</p>
+                    <p>Id produktu</p>
+                    <p>Nazwa produktu</p>
+                    <p>Cena</p>
+                    <p>Stan</p>
+                </div>
+                <div className="Dashboard__latest-item">
+                    <img
+                        src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78e78695-74ab-4162-b052-67f004aad13b/buty-do-golfa-air-max-270-g-3GkJ0N.png"
+                        alt="But"/>
+                    <span>354685-235465-126-156146</span>
+                    <span>Buty Nike Air90</span>
+                    <span>699</span>
+                    <span>3</span>
+                    {/*//todo Tutaj musi być wczytywanie z bazy danych za pomocą map*/}
+                </div>
+                <div className="Dashboard__latest-item">
+                    <img
+                        src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/78e78695-74ab-4162-b052-67f004aad13b/buty-do-golfa-air-max-270-g-3GkJ0N.png"
+                        alt="But"/>
+                    <span>354685-235465-126-156146</span>
+                    <span>Buty Nike Air90</span>
+                    <span>699</span>
+                    <span>3</span>
+                    {/*//todo Tutaj musi być wczytywanie z bazy danych za pomocą map*/}
                 </div>
             </div>
         </div>
