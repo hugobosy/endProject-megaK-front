eb112834e2ef9614a205d16b19cab72b74632be2
HugoBosy <hkliszcz@gmail.com>
2022-06-14 18:37:23 +0200

    fix styles Home add two section dashboard & add styles dashboard

--- src/pages/Dasboard/Dashboard.tsx
+++ src/pages/Dasboard/Dashboard.tsx
@@ -39,6 +39,7 @@ export const Dashboard = () => {
                         <p>(ilość uzytkowników)</p>
                         <p>Ostatnio zarejestrowany uzytkownik</p>
                     </div>
+                    {/*todo tutaj wczytywanie danych z bazy */}
                 </div>
             </div>
 
@@ -57,7 +58,7 @@ export const Dashboard = () => {
                         alt="But"/>
                     <span>354685-235465-126-156146</span>
                     <span>Buty Nike Air90</span>
-                    <span>699</span>
+                    <span>699zł</span>
                     <span>3</span>
                     {/*//todo Tutaj musi być wczytywanie z bazy danych za pomocą map*/}
                 </div>
@@ -67,8 +68,50 @@ export const Dashboard = () => {
                         alt="But"/>
                     <span>354685-235465-126-156146</span>
                     <span>Buty Nike Air90</span>
-                    <span>699</span>
+                    <span>699zł</span>
                     <span>3</span>
+                </div>
+            </div>
+
+            <div className="Dashboard__purchase-history">
+                <h2>Historia zakupów</h2>
+                <div className="Dashboard__purchase-item">
+                    <p>Id zakupu</p>
+                    <p>Produkty</p>
+                    <p>Dane kupującego</p>
+                    <p>Łączna cena</p>
+                    <p>Płatność</p>
+                </div>
+                <div className="Dashboard__purchase-item">
+                    <span>354685-235465-126-156146</span>
+                    <div>
+                        <p>Buty Nike Air90 <span>699zł</span></p>
+                        <p>Buty Nike Air90 <span>699zł</span></p>
+                        <p>Buty Nike Air90 <span>699zł</span></p>
+                    </div>
+                    <div>
+                        <span>Jan Kowalski</span>
+                        <span>Ćwiartki 3/4</span>
+                        <span>00-000 Warszawa</span>
+                    </div>
+                    <span>2097 zł</span>
+                    <span>w trakcie</span>
+                    {/*//todo Tutaj musi być wczytywanie z bazy danych za pomocą map*/}
+                </div>
+                <div className="Dashboard__purchase-item">
+                    <span>354685-235465-126-156146</span>
+                    <div>
+                        <p>Buty Nike Air90 <span>699zł</span></p>
+                        <p>Buty Nike Air90 <span>699zł</span></p>
+                        <p>Buty Nike Air90 <span>699zł</span></p>
+                    </div>
+                    <div>
+                        <span>Jan Kowalski</span>
+                        <span>Ćwiartki 3/4</span>
+                        <span>00-000 Warszawa</span>
+                    </div>
+                    <span>2097 zł</span>
+                    <span>w trakcie</span>
                     {/*//todo Tutaj musi być wczytywanie z bazy danych za pomocą map*/}
                 </div>
             </div>
