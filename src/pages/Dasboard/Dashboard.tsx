3d6892242dbda29d0fbc2259c82e6503702479fd
HugoBosy <hkliszcz@gmail.com>
2022-06-14 17:08:20 +0200

    remove one color from gradient, new folder Dashboard & change path this page, add style Dashboard

---
+++ src/pages/Dasboard/Dashboard.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {Basket, Dollar, User, Wallet} from "@styled-icons/boxicons-regular";
+import './Dasboard.css';
+
+export const Dashboard = () => {
+    return (
+        <div className="page">
+            <h1>Dashboard</h1>
+            <div className="Dashboard__base-info">
+                <div className="Dashboard__base-item">
+                    <span className="Dashboard__base-icon"><Basket size="30"/></span>
+                    <div className="Dashboard__base-content">
+                        <p>Ilość zamówień</p>
+                        <p>(liczba zamówień)</p>
+                        <p>Data ostatniego zamówienia</p>
+                    </div>
+                </div>
+                <div className="Dashboard__base-item">
+                    <span className="Dashboard__base-icon"><Dollar size="30"/></span>
+                    <div className="Dashboard__base-content">
+                        <p>Całkowity przychód</p>
+                        <p>(liczba przychodu)</p>
+                        <p>Przychód z ostatniego zamówienia</p>
+                    </div>
+                </div>
+                <div className="Dashboard__base-item">
+                    <span className="Dashboard__base-icon"><Wallet size="30"/></span>
+                    <div className="Dashboard__base-content">
+                        <p>Koszty</p>
+                        <p>(liczba kosztów)</p>
+                        <p>Nadchodzące płatności</p>
+                    </div>
+                </div>
+                <div className="Dashboard__base-item">
+                    <span className="Dashboard__base-icon"><User size="30"/></span>
+                    <div className="Dashboard__base-content">
+                        <p>Najnowsi użytkownicy</p>
+                        <p>(ilość uzytkowników)</p>
+                        <p>Ostatnio zarejestrowany uzytkownik</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
