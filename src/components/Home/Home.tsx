a7230e933f14318e332cbaec7fa04cbcf8e16fd8
HugoBosy <hkliszcz@gmail.com>
2022-06-14 10:01:44 +0200

    remove styles App add @styled-icons add assets image avatar

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { Menu } from "../Menu/Menu";
 import {Routing} from "../Routing/Routing";
+import {Bell, Envelope} from "@styled-icons/boxicons-regular";
+import man from '../../assets/img/man.png';
 
 export const Home = () => {
-
-
 
     return (
         <div className="Home">
             <header>
-
+                <form>
+                    <input type="search" placeholder="Search..."/>
+                </form>
+                <div>
+                    <Bell size="20"/>
+                </div>
+                <div>
+                    <Envelope size="20"/>
+                </div>
+                <div>
+                    <span><img src={man} alt="Avatar"/></span>
+                </div>
             </header>
             <aside>
                 <div>Logo</div>
