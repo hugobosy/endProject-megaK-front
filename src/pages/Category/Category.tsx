56c8a992a69175d2e9db2629ec450063b24594c7
HugoBosy <hkliszcz@gmail.com>
2022-06-15 13:03:51 +0200

    add structure page and file Category.css and first styles

--- src/pages/Category/Category.tsx
+++ src/pages/Category/Category.tsx
@@ -1,9 +1,101 @@
 import React from "react";
+import {Trash} from "@styled-icons/boxicons-solid";
+import './Category.css';
 
 export const Category = () => {
     return (
         <div className="page">
             <h1>Kategorie</h1>
+
+            <form>
+
+                <label>Dodaj nową kategorię</label>
+                <p>
+                    <input type="text"/>
+                </p>
+                <label>Dodaj adres odnośnika do obrazka reprezentującego kategorię</label>
+                <p>
+                    <input type="text"/>
+                </p>
+
+                <button type="submit">Dodaj</button>
+            </form>
+
+            <div className="Category">
+                <div className="Category__item">
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlXHYedLls8U5YX93oIYr804vdEGdLX0AS-Q&usqp=CAU"
+                        alt="plecak"/>
+                    <div className="Category__content">
+                        <h2>Plecaki</h2>
+                        <span><Trash size="30" color="red"/></span>
+                    </div>
+                </div>
+                <div className="Category__item">
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlXHYedLls8U5YX93oIYr804vdEGdLX0AS-Q&usqp=CAU"
+                        alt="plecak"/>
+                    <div className="Category__content">
+                        <h2>Plecaki</h2>
+                        <span><Trash size="30" color="red"/></span>
+                    </div>
+                </div>
+                <div className="Category__item">
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlXHYedLls8U5YX93oIYr804vdEGdLX0AS-Q&usqp=CAU"
+                        alt="plecak"/>
+                    <div className="Category__content">
+                        <h2>Plecaki</h2>
+                        <span><Trash size="30" color="red"/></span>
+                    </div>
+                </div>
+                <div className="Category__item">
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlXHYedLls8U5YX93oIYr804vdEGdLX0AS-Q&usqp=CAU"
+                        alt="plecak"/>
+                    <div className="Category__content">
+                        <h2>Plecaki</h2>
+                        <span><Trash size="30" color="red"/></span>
+                    </div>
+                </div>
+                <div className="Category__item">
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlXHYedLls8U5YX93oIYr804vdEGdLX0AS-Q&usqp=CAU"
+                        alt="plecak"/>
+                    <div className="Category__content">
+                        <h2>Plecaki</h2>
+                        <span><Trash size="30" color="red"/></span>
+                    </div>
+                </div>
+                <div className="Category__item">
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlXHYedLls8U5YX93oIYr804vdEGdLX0AS-Q&usqp=CAU"
+                        alt="plecak"/>
+                    <div className="Category__content">
+                        <h2>Plecaki</h2>
+                        <span><Trash size="30" color="red"/></span>
+                    </div>
+                </div>
+                <div className="Category__item">
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlXHYedLls8U5YX93oIYr804vdEGdLX0AS-Q&usqp=CAU"
+                        alt="plecak"/>
+                    <div className="Category__content">
+                        <h2>Plecaki</h2>
+                        <span><Trash size="30" color="red"/></span>
+                    </div>
+                </div>
+                <div className="Category__item">
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlXHYedLls8U5YX93oIYr804vdEGdLX0AS-Q&usqp=CAU"
+                        alt="plecak"/>
+                    <div className="Category__content">
+                        <h2>Plecaki</h2>
+                        <span><Trash size="30" color="red"/></span>
+                    </div>
+                </div>
+                {/*todo pobranie kategori z bazy*/}
+            </div>
         </div>
     )
 }
