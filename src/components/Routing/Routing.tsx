64c0bab2bb0c0614ff960796597b18e8b4fd486d
HugoBosy <hkliszcz@gmail.com>
2022-06-15 12:34:21 +0200

    add new folder Category and modified path

--- src/components/Routing/Routing.tsx
+++ src/components/Routing/Routing.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom";
 import {Dashboard} from "../../pages/Dasboard/Dashboard";
-import {Category} from "../../pages/Category";
+import {Category} from "../../pages/Category/Category";
 import {Clients} from "../../pages/Clients";
 import {Orders} from "../../pages/Orders/Orders";
 import {Products} from "../../pages/Products/Products";
