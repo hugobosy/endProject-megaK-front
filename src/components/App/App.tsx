088aeaa60dce6d649997a37388ad460d998a5ee5
HugoBosy <hkliszcz@gmail.com>
2022-06-15 15:40:55 +0200

    configure FE with types BE

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Test} from 'types';
 import {Home} from "../Home/Home";
 
+
 function App() {
+
+    const foo: Test = {
+        x: 'hugo'
+    }
+
   return (
     <div className="App">
       <Home/>
