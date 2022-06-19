7000c55f5a5ff73515cc815ec377b7fb1b5ad723
HugoBosy <hkliszcz@gmail.com>
2022-06-19 20:03:58 +0200

    add style add client

--- src/components/Clients/AddClient.tsx
+++ src/components/Clients/AddClient.tsx
@@ -20,35 +20,53 @@ export const AddClient = () => {
     return (
         <div className="Clients__add">
             <form className="Clients__add-form">
-                <label>Imię: </label>
-                <p><input type="text" value={addForm.name}/></p>
+                <h2>Dodaj nowego klienta</h2>
+                <div>
+                    <label>Imię: </label>
+                    <p><input type="text" value={addForm.name}/></p>
+                </div>
 
-                <label>Nazwisko: </label>
-                <p><input type="text" value={addForm.surname}/></p>
+                <div>
+                    <label>Nazwisko: </label>
+                    <p><input type="text" value={addForm.surname}/></p>
+                </div>
 
-                <label>Adres: </label>
-                <p><input type="text" value={addForm.address}/></p>
+                <div>
+                    <label>Adres: </label>
+                    <p><input type="text" value={addForm.address}/></p>
+                </div>
 
-                <label>Kod pocztowy: </label>
-                <p><input type="text" value={addForm.code}/></p>
+                <div>
+                    <label>Kod pocztowy: </label>
+                    <p><input type="text" value={addForm.code}/></p>
+                </div>
 
-                <label>Miejscowość: </label>
-                <p><input type="text" value={addForm.city}/></p>
+                <div>
+                    <label>Miejscowość: </label>
+                    <p><input type="text" value={addForm.city}/></p>
+                </div>
 
-                <label>Numer telefonu: </label>
-                <p><input type="number" value={addForm.phone}/></p>
+                <div>
+                    <label>Numer telefonu: </label>
+                    <p><input type="number" value={addForm.phone}/></p>
+                </div>
 
-                <label>Wybierz płeć:</label>
-                <p>
-                    <select value={addForm.gender}>
-                        <option value="-">-</option>
-                        <option value="Mężczyzna">Mężczyzna</option>
-                        <option value="Kobieta">Kobieta</option>
-                    </select>
-                </p>
-                <label>Email: </label>
-                <p><input type="email" value={addForm.email}/></p>
+                <div>
+                    <label>Wybierz płeć:</label>
+                    <p>
+                        <select value={addForm.gender}>
+                            <option value="-">-</option>
+                            <option value="Mężczyzna">Mężczyzna</option>
+                            <option value="Kobieta">Kobieta</option>
+                        </select>
+                    </p>
+                </div>
+                <div>
+                    <label>Email: </label>
+                    <p><input type="email" value={addForm.email}/></p>
+                </div>
                 <button type="submit">Dodaj klienta</button>
+                <span></span>
             </form>
         </div>
     )
