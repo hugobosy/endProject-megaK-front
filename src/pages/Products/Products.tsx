a7ba485b85cae6a1d33b2805c9f2fe4c6283a916
HugoBosy <hkliszcz@gmail.com>
2022-06-20 11:05:55 +0200

    add ProductList component

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './Products.css';
+import {ProductList} from "../../components/Products/ProductList";
 
 export const Products = () => {
     return (
@@ -26,105 +27,7 @@ export const Products = () => {
                 </form>
             </div>
 
-            <div className="Products__list">
-
-                <div className="Products__item">
-                    <img src="https://static.pepper.pl/threads/raw/bskQO/540238_1/re/768x768/qt/60/540238_1.jpg"
-                         alt="But"/>
-                    <div className="Products__desc">
-                        <h4>Buty Adidas</h4>
-                        <p>Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem
-                            ipsum dolor sit
-                        </p>
-                        <p>299zł</p>
-                        <div className="Products__btn">
-                            <button>Edytuj</button>
-                            <button>Usuń</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="Products__item">
-                    <img src="https://static.pepper.pl/threads/raw/bskQO/540238_1/re/768x768/qt/60/540238_1.jpg"
-                         alt="But"/>
-                    <div className="Products__desc">
-                        <h4>Buty Adidas</h4>
-                        <p>Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem
-                            ipsum dolor sit
-                        </p>
-                        <p>299zł</p>
-                        <div className="Products__btn">
-                            <button>Edytuj</button>
-                            <button>Usuń</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="Products__item">
-                    <img src="https://static.pepper.pl/threads/raw/bskQO/540238_1/re/768x768/qt/60/540238_1.jpg"
-                         alt="But"/>
-                    <div className="Products__desc">
-                        <h4>Buty Adidas</h4>
-                        <p>Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem
-                            ipsum dolor sit
-                        </p>
-                        <p>299zł</p>
-                        <div className="Products__btn">
-                            <button>Edytuj</button>
-                            <button>Usuń</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="Products__item">
-                    <img src="https://static.pepper.pl/threads/raw/bskQO/540238_1/re/768x768/qt/60/540238_1.jpg"
-                         alt="But"/>
-                    <div className="Products__desc">
-                        <h4>Buty Adidas</h4>
-                        <p>Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem
-                            ipsum dolor sit
-                        </p>
-                        <p>299zł</p>
-                        <div className="Products__btn">
-                            <button>Edytuj</button>
-                            <button>Usuń</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="Products__item">
-                    <img src="https://static.pepper.pl/threads/raw/bskQO/540238_1/re/768x768/qt/60/540238_1.jpg"
-                         alt="But"/>
-                    <div className="Products__desc">
-                        <h4>Buty Adidas</h4>
-                        <p>Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem
-                            ipsum dolor sit
-                        </p>
-                        <p>299zł</p>
-                        <div className="Products__btn">
-                            <button>Edytuj</button>
-                            <button>Usuń</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="Products__item">
-                    <img src="https://static.pepper.pl/threads/raw/bskQO/540238_1/re/768x768/qt/60/540238_1.jpg"
-                         alt="But"/>
-                    <div className="Products__desc">
-                        <h4>Buty Adidas</h4>
-                        <p>Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem
-                            ipsum dolor sit
-                        </p>
-                        <p>299zł</p>
-                        <div className="Products__btn">
-                            <button>Edytuj</button>
-                            <button>Usuń</button>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
+            <ProductList/>
 
         </div>
     )
