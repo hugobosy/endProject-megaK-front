623794b80ed6c7eb23a73f8bbb0952e7b3c5ab04
HugoBosy <hkliszcz@gmail.com>
2022-06-23 14:56:01 +0200

    fix send data to back-end

--- src/components/Products/EditProduct.tsx
+++ src/components/Products/EditProduct.tsx
@@ -40,33 +40,49 @@ export const EditProduct = (props: Props) => {
     useEffect(() => {
         getOne(props.id)
         getCategory();
+        setData({...data, id: props.id})
     }, [])
+
+    const id = props.id;
+
+    const handleEdit = async() => {
+        await fetch(`http://localhost:3001/products/edit/${id}`, {
+            method: 'POST',
+            body: JSON.stringify({data}),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    }
 
     const handleEditSubmit = (e: SyntheticEvent) => {
         e.preventDefault()
+        handleEdit()
+
     }
 
+    console.log(data)
 
     return (
         <div className="Products__add">
             {productOne.map(product => (
-                <form className="Products__add-form" onSubmit={handleEditSubmit}>
+                <form className="Products__add-form" onSubmit={handleEditSubmit} id={product.id} key={product.id}>
                     <div>
                         <label>Nazwa firmy: </label>
-                        <p><input type="text" value={product.firm} onChange={e => setData({...data, firm: e.target.value})}/>
+                        <p><input type="text" value={data.firm} placeholder={product.firm} onChange={e => setData({...data, firm: e.target.value})}/>
                         </p>
                     </div>
 
                     <div>
                         <label>Model produktu: </label>
-                        <p><input type="text" value={product.model} onChange={e => setData({...data, model: e.target.value})}/>
+                        <p><input type="text" value={data.model} placeholder={product.model} onChange={e => setData({...data, model: e.target.value})}/>
                         </p>
                     </div>
 
                     <div>
                         <label>Kategoria: </label>
                         <p>
-                            <select value={product.category} onChange={e => setData({...data, category: e.target.value})}>
+                            <select value={data.category} onChange={e => setData({...data, category: e.target.value})}>
                                 <option value={"-"}>-</option>
                                 {category.map(cat => (
                                     <option value={cat.name} key={cat.id}>{cat.name}</option>
@@ -93,35 +109,35 @@ export const EditProduct = (props: Props) => {
 
                     <div>
                         <label>Kolor: </label>
-                        <p><input type="text" value={product.color} onChange={e => setData({...data, color: e.target.value})}/>
+                        <p><input type="text" value={data.color} placeholder={product.color} onChange={e => setData({...data, color: e.target.value})}/>
                         </p>
                     </div>
 
                     <div>
                         <label>Ilość: </label>
-                        <p><input type="number" value={product.quantity}
+                        <p><input type="number" value={data.quantity}
                                   onChange={e => setData({...data, quantity: Number(e.target.value)})}/></p>
                     </div>
 
                     <div className="Products__add-textarea">
                         <label>Opis produktu: </label>
-                        <p><textarea value={product.description}
+                        <p><textarea value={data.description} placeholder={product.description}
                                      onChange={e => setData({...data, description: e.target.value})}></textarea></p>
                     </div>
 
                     <div>
                         <label>Link do zdjęcia: </label>
-                        <p><input type="text" value={product.picture}
+                        <p><input type="text" value={data.picture} placeholder={product.picture}
                                   onChange={e => setData({...data, picture: e.target.value})}/></p>
                     </div>
 
                     <div>
                         <label>Cena: </label>
-                        <p><input type="number" value={product.price}
+                        <p><input type="number" value={data.price}
                                   onChange={e => setData({...data, price: Number(e.target.value)})}/></p>
                     </div>
 
-                    <button type="submit">Dodaj produkt</button>
+                    <button type="submit">Edytuj produkt</button>
                     <span onClick={() => props.close(false)}></span>
                 </form>
             ))}
