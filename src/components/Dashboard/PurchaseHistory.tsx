4866fdf7c6d5cb7e4a920239e6a26dff6d2827fe
HugoBosy <hkliszcz@gmail.com>
2022-06-30 12:53:00 +0200

    add style purchase history & add button order when quantity product is 0

--- src/components/Dashboard/PurchaseHistory.tsx
+++ src/components/Dashboard/PurchaseHistory.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {Order} from "types";
+
 interface Props {
     orders: Order[]
 }
@@ -15,34 +16,19 @@ export const PurchaseHistory = (props: Props) => {
                 <p>Łączna cena</p>
                 <p>Płatność</p>
             </div>
-
-            {/*    <span>354685-235465-126-156146</span>*/}
-            {/*    <div>*/}
-            {/*        <p>Buty Nike Air90 <span>699zł</span></p>*/}
-            {/*        <p>Buty Nike Air90 <span>699zł</span></p>*/}
-            {/*        <p>Buty Nike Air90 <span>699zł</span></p>*/}
-            {/*    </div>*/}
-            {/*    <div>*/}
-            {/*        <span>Jan Kowalski</span>*/}
-            {/*        <span>Ćwiartki 3/4</span>*/}
-            {/*        <span>00-000 Warszawa</span>*/}
-            {/*    </div>*/}
-            {/*    <span>2097 zł</span>*/}
-            {/*    <span>w trakcie</span>*/}
-                {/*//todo Tutaj musi być wczytywanie z bazy danych za pomocą map*/}
-                {props.orders.map(order => (
-                    <div className="Dashboard__purchase-item">
-                        <span>{order.id}</span>
-                        <div>
-                            {order.products.split(', ').map(item => <p>{item}</p>)}
-                        </div>
-                        <div>
-                            {order.client.split(', ').map(item => <p>{item}</p>)}
-                        </div>
-                        <span>{order.total}</span>
-                        <span>{order.payment}</span>
+            {props.orders.map(order => (
+                <div className="Dashboard__purchase-item">
+                    <span>{order.id}</span>
+                    <div>
+                        {order.products.split(', ').map(item => <p>{item}</p>)}
                     </div>
-                ))}
+                    <div>
+                        {order.client.split(', ').map(item => <p>{item}</p>)}
+                    </div>
+                    <span>{order.total}</span>
+                    <span>{order.payment ? 'Opłacone' : 'W trakcie'}</span>
+                </div>
+            ))}
 
         </div>
     )
--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -17,18 +17,19 @@ export const ProductList = (props: Props) => {
                         <img src={product.picture}
                              alt="But"/>
                     </div>
-                    <div className="Products__desc" style={product.quantity ? {background: "#ffffff"} : {background: "#ff0000", color: "#fff"}}>
+                    <div className="Products__desc" style={product.quantity ? {background: "#ffffff"} : {background: "#d37878", color: "#fff"}}>
                         <h2>{product.firm}</h2>
                         <h5>{product.model}</h5>
                         <p>{product.category}</p>
-                        <p>{product.description.slice(0, 90)}...Czytaj więcej</p>
+                        <p>{product.description.slice(0, 90)}... Czytaj więcej</p>
                         <p><span>Pozostałych sztuk: </span> {product.quantity}</p>
                         <p>{product.price} zł</p>
                         <div className="Products__btn">
                             <button onClick={props.edit} id={product.id}>Edytuj</button>
                             <button onClick={props.delete} id={product.id} data-name={`${product.firm} ${product.model}`}>Usuń</button>
-                        {/*    todo dodaj tutaj przycisk do symulacji zamówienia*/}
+                            {product.quantity ? null : <button id={product.id}>Zamów</button>}
                         </div>
+                    {/*    todo dodaj potierdzenia do usunięcia produktu*/}
                     </div>
                 </div>
             ))}
